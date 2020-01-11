fa52c6774f7346bb23a3758d3c31bc42cc6a8815
KappaHDEnsoul <w0lfelosharp@hotmail.com>
2020-01-11 15:04:35 +0100

    First Update
    First Update

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LoadEncrypt
+{
+    using System;
+    using System.Reflection;
+    using System.Security.Permissions;
+
+    using EnsoulSharp.SDK;
+
+    using RebornAIO.Properties;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            try
+            {
+                var a = Assembly.Load(Resources.RebornAIO);
+                var myType = a.GetType("RebornAIO.Program");// namespace + class name
+                var methon = myType.GetMethod("Main", BindingFlags.Public | BindingFlags.Static); // methon
+
+                if (methon != null)
+                {
+                    methon.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
